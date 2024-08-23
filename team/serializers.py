40f069ce67a7dc58a32d537e8bfda2408e55e164
optiprime27 <gim-yesong@gim-yesong-ui-MacBookPro.local>
2024-08-24 05:47:19 +0900

    team create 수정

--- team/serializers.py
+++ team/serializers.py
@@ -7,46 +7,53 @@ class TeamNameSerializer(serializers.ModelSerializer):
         model = Team
         fields = ['teamName']
 
-class TeamSerializer(serializers.ModelSerializer):
-    leader = serializers.CharField(source='leader.nickname')
-    leaderTier = serializers.CharField(source='leader.tier')
+# class TeamSerializer(serializers.ModelSerializer):
+#     leader = serializers.CharField(source='leader.nickname')
+#     leaderTier = serializers.CharField(source='leader.tier')
     
-    member2 = serializers.CharField(source='member2.nickname', allow_null=True)
-    member2Tier = serializers.CharField(source='member2.tier', allow_null=True)
+#     member2 = serializers.CharField(source='member2.nickname', allow_null=True)
+#     member2Tier = serializers.CharField(source='member2.tier', allow_null=True)
 
-    member3 = serializers.CharField(source='member3.nickname', allow_null=True)
-    member3Tier = serializers.CharField(source='member3.tier', allow_null=True)
+#     member3 = serializers.CharField(source='member3.nickname', allow_null=True)
+#     member3Tier = serializers.CharField(source='member3.tier', allow_null=True)
 
-    member4 = serializers.CharField(source='member4.nickname', allow_null=True)
-    member4Tier = serializers.CharField(source='member4.tier', allow_null=True)
+#     member4 = serializers.CharField(source='member4.nickname', allow_null=True)
+#     member4Tier = serializers.CharField(source='member4.tier', allow_null=True)
 
-    member5 = serializers.CharField(source='member5.nickname', allow_null=True)
-    member5Tier = serializers.CharField(source='member5.tier', allow_null=True)
+#     member5 = serializers.CharField(source='member5.nickname', allow_null=True)
+#     member5Tier = serializers.CharField(source='member5.tier', allow_null=True)
     
 
-    class Meta:
-        model = Team
-        fields = [
-            'teamName', 
-            'school', 
-            'teamTier',
-            'leader', 
-            'leaderTier', 
-            'member2', 
-            'member2Tier', 
-            'member3', 
-            'member3Tier', 
-            'member4', 
-            'member4Tier', 
-            'member5', 
-            'member5Tier'
-        ]
+#     class Meta:
+#         model = Team
+#         fields = [
+#             'teamName', 
+#             'school', 
+#             'teamTier',
+#             'leader', 
+#             'leaderTier', 
+#             'member2', 
+#             'member2Tier', 
+#             'member3', 
+#             'member3Tier', 
+#             'member4', 
+#             'member4Tier', 
+#             'member5', 
+#             'member5Tier'
+#         ]
 
 class TeamRequestSerializer(serializers.ModelSerializer):
+    leader = serializers.CharField()
+    member2 = serializers.CharField()
+    member3 = serializers.CharField()
+    member4 = serializers.CharField()
+    member5 = serializers.CharField()
+
+    
     class Meta:
         model = Team
         fields = [
-            'teamName', 
+            'teamame', 
             'school', 
             'leader', 
             'member2', 
@@ -55,8 +62,5 @@ class TeamRequestSerializer(serializers.ModelSerializer):
             'member5'
         ]
     
-    def create(self, validated_data):
-        team = Team.objects.create(**validated_data)
-        return team
-            
+    
 
