174885c27655247f5b979f6a6d71e7f3096b64b0
lee-youn <5816654@naver.com>
2024-08-24 00:56:55 +0900

    Feat : match 조회 구현

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from match.views import MatchRegistData, MatchList
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls")),
+    path("api/match/", MatchRegistData),
+    path("api/match/all/", MatchList)
 ]
--- match/views.py
+++ match/views.py
@@ -8,10 +8,10 @@ from user.models import User
 from serializers import MatchSerializer
 
 
-#경기 등록 초기 데이터
+#경기 등록 데이터
 @api_view(['POST','GET'])
 @permission_classes([IsAuthenticated])
-def TeamData(request):
+def MatchRegistData(request):
     if request.method == 'GET':
         user = request.user
         school = user.school
@@ -32,6 +32,13 @@ def TeamData(request):
             return Response({"status": "error", "message": "유효하지 않은 데이터입니다", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = serializer.validated_data()
+        
+        try:
+            MatchGroup = Match.objects.filter(user=user, teamName = serializer.get("teamName"))
+        except MatchGroup.Exist:
+            return Response({"message : 해당 팀으로 등록할 수 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+
         try:
             MatchGroup = Match.objects.filter(user=user, matchDate = serializer.get("matchDate"))
         except MatchGroup.Exist:
@@ -43,15 +50,39 @@ def TeamData(request):
             school = serializer.get("school"),
             matchDate = serializer.get("matchDate"),
             teamScore = serializer.get("teamScore"),
+            openChatUrl = serializer.get("openChatUrl")
         )
         return Response(match, status = status.HTTP_201_CREATED)
 
-        
 
+#경기 리스트 데이터
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def MatchList(request, pk):
+    if request.method == 'GET':
+        user = request.user
+        matches = Match.objects.filter(user=user, many=True)
 
+        # Prepare the list of formatted memos
+        formatted_matches = []
+        for match in matches:
+            formatted_match = {
+            "id": match.id,
+            "teamName": match.teamName,
+            "school": match.school,  # Adjust according to your actual model field names
+            "matchDate": match.matchDate,  # Adjust according to your actual model field names
+            "teamScore": match.teamScore,  # Adjust according to your actual model field names
+            "openChatUrl": match.openChatUrl,  # Adjust according to your actual model field names
+            }
+            formatted_matches.append(formatted_match)
 
-        
+        response_data = {
+        "detail": "경기가 성공적으로 조회되었습니다.",
+        "data":
+            {
+                "memo": formatted_matches
+            }
+        }
 
+    return Response(response_data, status=status.HTTP_200_OK)
 
-        
-        
